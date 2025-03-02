cc5f4af93982f3a3da82ba2d0967906ccfb9759c
mariinkys <kysgeneral.nrgua@slmail.me>
2025-03-02 09:07:05 +0100

    fix: products table not containing category_id

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -6,13 +6,15 @@ CREATE TABLE product_categories (
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
--- Create Product table
+-- Create Product table 
 CREATE TABLE products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    category_id INTEGER,
     name TEXT NOT NULL,
     price REAL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES product_categories (id)
 );
 
 -- Trigger to update 'updated_at' on ProductCategory
@@ -20,7 +22,9 @@ CREATE TRIGGER update_product_category_updated_at
 AFTER UPDATE ON product_categories
 FOR EACH ROW
 BEGIN
-    UPDATE product_categories SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id;
+    UPDATE product_categories
+    SET updated_at = CURRENT_TIMESTAMP
+    WHERE id = OLD.id;
 END;
 
 -- Trigger to update 'updated_at' on Product
@@ -28,5 +32,7 @@ CREATE TRIGGER update_product_updated_at
 AFTER UPDATE ON products
 FOR EACH ROW
 BEGIN
-    UPDATE products SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id;
+    UPDATE products
+    SET updated_at = CURRENT_TIMESTAMP
+    WHERE id = OLD.id;
 END;
