87a8714cc30b53519f17fc961ec78cabfa83f80d
mariinkys <kysgeneral.nrgua@slmail.me>
2025-03-04 08:46:19 +0100

    feat: add basic seeding

---
+++ migrations/0002_seeding.sql
@@ -0,0 +1,8 @@
+-- Seed product categories
+INSERT INTO product_categories (name, is_deleted) VALUES ('Carne', 0);
+INSERT INTO product_categories (name, is_deleted) VALUES ('Postre', 0);
+
+-- Seed products
+INSERT INTO products (category_id, name, inside_price, outside_price, is_deleted) VALUES (1, 'Filete', 9.80, 9.90, 0);
+INSERT INTO products (category_id, name, inside_price, outside_price, is_deleted) VALUES (1, 'Pechuga de Coco', 1.00, 1.50, 0);
+INSERT INTO products (category_id, name, inside_price, outside_price, is_deleted) VALUES (2, 'Crema de Lima', 1.20, 1.30, 0);
